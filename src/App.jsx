924e11d2b689f6a261e9120567ac99d41bd696f5
Sekiguchi-Atsushi-WEB <aaa.sekiguchi@gmail.com>
2025-11-25 16:30:48 +0900

    29. Atomの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { PrimaryButton } from "./components/atoms/buttons/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/buttons/SecondaryButton";
 import "./style.css";
 
 function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div className="App">
+      <PrimaryButton>テスト</PrimaryButton>
+      <SecondaryButton>検索</SecondaryButton>
+    </div>
   );
 }
 
